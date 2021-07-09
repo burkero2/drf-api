2c1317e870404eeb751e8189fa97120da3e82725
burkero2 <ronanburke@codeinstitute.net>
2021-07-09 14:42:30 +0000

    Challenge 011: Comment Serializer

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Post
 
 # Create your tests here.
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='a', password='pass')
+    
+    def test_can_list_posts(self):
+        a = User.objects.get(username='a')
+        Post.objects.create(owner=a, title='a title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data, len(response.data))
