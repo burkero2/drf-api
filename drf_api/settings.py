5fba221ba928ad065bbca1084f18929e90f4981a
burkero2 <ronanburke@codeinstitute.net>
2021-08-09 13:39:41 +0000

    d-003

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -136,7 +136,7 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': ({
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     } if 'DEV' in os.environ else dj_database_url.parse(
