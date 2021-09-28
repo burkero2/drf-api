d822f5fc792c72e1ded00f73c4324f95dfb356db
burkero2 <ronanburke@codeinstitute.net>
2021-09-28 15:42:11 +0000

    Added Client_Origin_Dev

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -106,7 +106,8 @@ MIDDLEWARE = [
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+        os.environ.get('CLIENT_ORIGIN'), 
+        os.environ.get('CLIENT_ORIGIN_DEV'),
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
